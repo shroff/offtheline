0151cb7a6f82bebabe331e17b41c163416c5f5dc
Abhishek Shroff <shroff@kudosoft.net>
2020-06-03 10:40:01 +0530

    Move flow to appcore

---
+++ lib/flow/flow.dart
@@ -0,0 +1,179 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+abstract class FlowManager<T> extends StatefulWidget {
+  final String title;
+
+  const FlowManager({
+    Key key,
+    @required this.title,
+  }) : super(key: key);
+
+  FlowManagerArgs<T> flowArgs(BuildContext context) =>
+      ModalRoute.of(context).settings.arguments;
+
+  static _FlowManagerState<T> of<T>(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<InheritedFlowManager>()
+        .data;
+  }
+
+
+  FutureOr<bool> confirmDropFlow(BuildContext context) {
+    return true;
+  }
+
+  FutureOr<bool> finishFlow(BuildContext context, T data);
+
+  @override
+  State<StatefulWidget> createState() => _FlowManagerState<T>();
+}
+
+class FlowManagerArgs<T> {
+  final Map<String, FlowStep<T>> steps;
+  final String initialStep;
+  final T initialData;
+  final String finishText;
+
+  FlowManagerArgs(
+    this.steps,
+    this.initialStep,
+    this.initialData, {
+    this.finishText = 'FINISH',
+  });
+}
+
+class FlowStep<T> {
+  final Widget Function(BuildContext, GlobalKey<State>) buildPage;
+  final Function(T, BuildContext) setStateOnStart;
+  final String Function(T, BuildContext) generateNextStepName;
+  final String Function(T, BuildContext) generateNextButtonText;
+  final bool Function(T, BuildContext) nextEnabled;
+  final FutureOr<bool> Function(T, BuildContext) onFinish;
+
+  FlowStep({
+    @required this.buildPage,
+    this.generateNextStepName,
+    this.generateNextButtonText,
+    this.setStateOnStart = _doNothing,
+    this.nextEnabled = _doNothing,
+    this.onFinish = _doNothing,
+  });
+
+  static bool _doNothing(data, context) => true;
+}
+
+class InheritedFlowManager extends InheritedWidget {
+  final _FlowManagerState data;
+
+  InheritedFlowManager({
+    Key key,
+    @required this.data,
+    @required Widget child,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedFlowManager old) => old != this;
+}
+
+class _FlowManagerState<T> extends State<FlowManager> {
+  final PageStorageBucket _bucket = PageStorageBucket();
+
+  List<FlowStep<T>> steps;
+  T data;
+
+  FlowStep<T> get currentStep => steps[steps.length - 1];
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final args = widget.flowArgs(context);
+    steps = [args.steps[args.initialStep]];
+    data = args.initialData;
+    setState(() {
+      currentStep.setStateOnStart(data, context);
+    });
+  }
+
+  void updateState(Function() fn) {
+    setState(fn);
+  }
+
+  void nextStep() async {
+    if (await currentStep.onFinish(data, context)) {
+      if (currentStep.generateNextStepName == null) {
+        if ((await widget.finishFlow(context, data)) ?? false) {
+          print('Finish');
+          Navigator.of(context).pop(data);
+        }
+      } else {
+        final stepName = currentStep.generateNextStepName(data, context);
+        final step = widget.flowArgs(context).steps[stepName];
+        print('Next to $stepName');
+        if (step == null) throw "Unknown next state: $step";
+
+        setState(() {
+          steps.add(step);
+          currentStep.setStateOnStart(data, context);
+        });
+      }
+    }
+  }
+
+  bool canPopStep() {
+    return steps.length > 1;
+  }
+
+  Future<bool> _onBackPress() async {
+    steps.removeLast();
+    if (steps.isEmpty) {
+      print('Back Out');
+      if (await widget.confirmDropFlow(context)) Navigator.of(context).pop();
+    } else {
+      print('Back');
+      setState(() {
+        currentStep.setStateOnStart(data, context);
+      });
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InheritedFlowManager(
+      data: this,
+      child: WillPopScope(
+        onWillPop: _onBackPress,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            leading: IconButton(
+              icon: Icon(canPopStep() ? Icons.arrow_back : Icons.close),
+              onPressed: _onBackPress,
+            ),
+            actions: <Widget>[
+              if (currentStep.nextEnabled != null)
+                FlatButton(
+                  child: Text(currentStep.generateNextStepName == null
+                      ? widget.flowArgs(context).finishText
+                      : currentStep.generateNextButtonText
+                              ?.call(data, context) ??
+                          'NEXT'),
+                  textColor: Colors.white,
+                  textTheme: ButtonTextTheme.primary,
+                  onPressed:
+                      currentStep.nextEnabled(data, context) ? nextStep : null,
+                ),
+            ],
+          ),
+          body: PageStorage(
+            bucket: _bucket,
+            child: Builder(
+                builder: (context) => currentStep.buildPage(context, null)),
+          ),
+        ),
+      ),
+    );
+  }
+}
