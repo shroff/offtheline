50b65529fd06b74d6a034ee1872522c2c3d92dd6
Abhishek Shroff <shroff@kudosoft.net>
2020-11-02 11:04:25 +0530

    Upgrade flutter packages

--- lib/filepicker/filepicker_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'picked_file_data.dart';
-
-Future<PickedFileData> pickOrCapture(BuildContext context) => throw UnsupportedError(
-    'Cannot create a client without dart:html or dart:io.');
