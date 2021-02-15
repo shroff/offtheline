a7ac2eca4f83d183c8b2b072787fbbb9829f0379
Abhishek Shroff <shroff@kudosoft.net>
2021-02-16 00:00:01 +0530

    Fix core export

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,9 +1,7 @@
 library core;
 
-import 'core_stub.dart'
+export 'core_stub.dart'
 // ignore: uri_does_not_exist
     if (dart.library.html) 'core_browser.dart'
 // ignore: uri_does_not_exist
     if (dart.library.io) 'core_mobile.dart';
-
-export 'core_stub.dart';
