fb2bc054f973563378dbcd153101311dc6de2764
Abhishek Shroff <shroff@kudosoft.net>
2021-06-21 10:30:35 +0530

    Exponential Backoff in ticker connect

--- lib/core/ticker_sync_cubit.dart
+++ lib/core/ticker_sync_cubit.dart
@@ -1,20 +1,32 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:appcore/core/api.dart';
+import 'package:async/async.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:uri/uri.dart';
 
+const _delays = [0, 2, 4, 8, 16, 32, 64];
+
 abstract class TickerSyncCubit extends Cubit<TickerSyncState> {
   final ApiCubit api;
 
-  Future<WebSocket>? _socketFuture;
-  StreamSubscription<dynamic>? socketSubscription;
+  late final void Function(dynamic) _successfulResponseProcessor = (response) {
+    if (state is TickerSyncStateDisconnected) {
+      connect();
+    }
+  };
+
+  CancelableOperation<bool>? _delayOperation;
+  CancelableOperation<WebSocket>? _connectOperation;
+  WebSocket? _socket;
+  StreamSubscription<dynamic>? _socketSubscription;
 
   TickerSyncCubit(
     this.api,
-  ) : super(const TickerSyncStateDisconnected()) {
+  ) : super(const TickerSyncStateDisconnected(0)) {
     api.stream.listen((apiState) {
       if (apiState.session == null) {
         debugPrint('[sync] Logging Out');
@@ -23,12 +35,21 @@ abstract class TickerSyncCubit extends Cubit<TickerSyncState> {
         connect();
       }
     });
+
+    stream.listen((TickerSyncState state) {
+      if (state is TickerSyncStateDisconnected && state.attempt > 0) {
+        _connect(state.attempt);
+      }
+    });
+    api.addResponseProcessor(_successfulResponseProcessor);
+
     connect();
   }
 
   @override
   Future<void> close() async {
-    debugPrint('[sync] Closing');
+    debugPrint('[sync] BLoC close');
+    api.removeResponseProcessor(_successfulResponseProcessor);
     disconnect('close');
     await super.close();
   }
@@ -36,8 +57,40 @@ abstract class TickerSyncCubit extends Cubit<TickerSyncState> {
   UriBuilder createUriBuilder();
 
   void connect() async {
-    debugPrint('[sync] Connect called');
-    // * Make sure we are ready
+    debugPrint('[sync] Connect');
+    _connect(0);
+  }
+
+  void disconnect(String reason) {
+    debugPrint('[sync] Disconnect');
+    //TODO: test timeout behavior
+    _delayOperation?.cancel();
+    _connectOperation?.cancel();
+    _socketSubscription?.cancel();
+    _socket?.close(1001, reason);
+    emit(const TickerSyncStateDisconnected(-1));
+  }
+
+  void _connect(int attempt) async {
+    debugPrint('[sync] Connection attempt $attempt');
+
+    _delayOperation?.cancel();
+    if (attempt > 0) {
+      // Exponential backoff delay
+      int delaySeconds = _delays[min(attempt, _delays.length - 1)];
+      debugPrint('Waiting for $delaySeconds seconds');
+      final delay = CancelableOperation.fromFuture(
+          Future.delayed(Duration(seconds: delaySeconds), () => true));
+      _delayOperation = delay;
+      final delaySuccess = await delay.valueOrCancellation(false) ?? false;
+      if (!delaySuccess) {
+        debugPrint('Delay canceled: $delaySeconds');
+        return;
+      }
+      _delayOperation = null;
+      debugPrint('Delay finished: $delaySeconds');
+    }
+
     if (api.state is! ApiStateLoggedIn ||
         state is! TickerSyncStateDisconnected) {
       return;
@@ -49,56 +102,43 @@ abstract class TickerSyncCubit extends Cubit<TickerSyncState> {
     debugPrint('[sync] Connecting');
     emit(const TickerSyncStateConnecting());
 
-    // ignore: close_sinks
-    _socketFuture = WebSocket.connect(
+    final session = api.state.session;
+    final operation = CancelableOperation.fromFuture(WebSocket.connect(
       uriBuilder.toString(),
       headers: api.headers,
-    );
+    ));
 
-    final session = api.state.session;
-    socketSubscription = await _socketFuture!.then((socket) {
-      debugPrint('[sync] Connected');
-      emit(const TickerSyncStateConnected());
+    try {
+      _connectOperation = operation;
+      _socket = await operation.valueOrCancellation();
 
-      return socket.listen((message) {
+      _socketSubscription = _socket?.listen((message) {
         api.parseResponseString(message, requestSession: session);
       }, onError: (err) {
-        _socketFuture = null;
+        final socket = _socket;
+        _socket = null;
+        socket?.close(
+          1002,
+        );
         debugPrint("[sync] Listen error: $err");
-        emit(const TickerSyncStateDisconnected());
+        emit(const TickerSyncStateDisconnected(1));
       }, onDone: () {
-        _socketFuture = null;
-        debugPrint('[sync] Closed: ${socket.closeCode}, ${socket.closeReason}');
-        emit(const TickerSyncStateDisconnected());
-        if (socket.closeCode != 1001) {
-          // TODO: Exponential backoff
-          Future.delayed(Duration(seconds: 1), () {
-            connect();
-          });
-        }
+        // debugPrint('[sync] Closed: ${socket.closeCode}, ${socket.closeReason}');
+        emit(TickerSyncStateDisconnected(-1));
       });
-    }, onError: (err) {
-      debugPrint("[sync] Socket connection error: $err");
-      _socketFuture = null;
-      emit(TickerSyncStateDisconnected());
-      return null;
-    });
+
+      debugPrint('[sync] Connected');
+      emit(const TickerSyncStateConnected());
+    } catch (err) {
+      _socket = null;
+      debugPrint("[sync] Connection error: $err");
+      emit(TickerSyncStateDisconnected(attempt + 1));
+    } finally {
+      _connectOperation = null;
+    }
   }
 
   UriBuilder addParams(UriBuilder builder) => builder;
-
-  void disconnect(String reason) {
-    //TODO: test timeout behavior
-    try {
-      _socketFuture
-          ?.timeout(Duration.zero)
-          .then((socket) => socket.close(1001, reason));
-      socketSubscription?.cancel();
-    } on TimeoutException {
-      _socketFuture = null;
-      emit(TickerSyncStateDisconnected());
-    }
-  }
 }
 
 abstract class TickerSyncState {
@@ -106,7 +146,9 @@ abstract class TickerSyncState {
 }
 
 class TickerSyncStateDisconnected extends TickerSyncState {
-  const TickerSyncStateDisconnected();
+  final int attempt;
+
+  const TickerSyncStateDisconnected(this.attempt);
 }
 
 class TickerSyncStateConnecting extends TickerSyncState {
